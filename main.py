e5cde0a6144b551834810c45c63ef544fa2e629c
LLLida <0adilmohammad0@gmail.com>
2024-03-04 00:00:49 +0300

    Использование ООП
    Вынес логику игры тетрис в отдельный файл. Такое ощущение, что теперь
    можно обучать агента.

--- main.py
+++ main.py
@@ -2,103 +2,15 @@ import pygame
 import random
 import numpy as np
 
+from tetris.game import TetrisGame
+
 pygame.init()
 
-game_width, game_height = 12, 26
+tetris = TetrisGame(width=12, height=26)
+
 cell_size = 30
-top_margin = 4                  # чтобы фигуры плавно входили на экран
-game_grid = np.zeros((top_margin+game_height, game_width), dtype=np.int32)
 
-piece_shapes = [
-    # квадратик
-    [[1, 1],
-     [1, 1]],
-    # ступенька вправо
-    [[0, 1, 1],
-     [1, 1]],
-    # ступенька влево
-    [[1, 1],
-     [0, 1, 1]],
-    # палочка
-    [[1, 1, 1, 1]],
-    # палочка с крюком справа
-    [[0, 0, 1],
-     [1, 1, 1]],
-    # палочка с крюком слева
-    [[1, 0, 0],
-     [1, 1, 1]],
-    # треугольник
-    [[0, 1, 0],
-     [1, 1, 1]]
-]
-
-def spawn_piece():
-    sx, sy = game_width//2, top_margin-1
-    shape = random.choice(piece_shapes)
-
-    blocks = []
-    for i in range(len(shape)):
-        for j in range(len(shape[i])):
-            if shape[i][j] != 0:
-                blocks.append((sx+j, sy+i))
-    return blocks
-
-def piece_pos(piece):
-    x = sum([b[0] for b in piece]) / len(piece)
-    y = sum([b[1] for b in piece]) / len(piece)
-    return int(x), int(y)
-
-def rotate_piece(piece):
-    mx, my = piece_pos(piece)
-
-    new_piece = [(mx + (y-my), my + (mx-x)) for x, y in piece]
-    for x, y in new_piece:
-        if x >= game_width or x < 0 or y >= game_height:
-            return
-        # if not (x, y) in new_piece and game_grid[y, x] != 0:
-        #     return
-
-    for i in range(len(piece)):
-        x, y = piece[i]
-        game_grid[y, x] = 0
-
-    for i in range(len(piece)):
-        x, y = piece[i]
-        piece[i] = (mx + (y-my), my + (mx-x))
-
-    for i in range(len(piece)):
-        x, y = piece[i]
-        game_grid[y, x] = 3
-
-def move_piece(piece, dx, dy) -> bool:
-    min_x = np.min([b[0] for b in piece])
-    max_x = np.max([b[0] for b in piece])
-    max_y = np.max([b[1] for b in piece])
-    if max_x+dx > game_width-1 or min_x+dx < 0:
-        return False
-    if max_y >= game_height+top_margin-1:
-        return True
-
-    can_move = True
-    for x, y in piece:
-        if not (x+dx, y+dy) in piece and game_grid[y+dy, x+dx] != 0:
-            can_move = False
-            break
-    if can_move:
-        for i in range(len(piece)):
-            x, y = piece[i]
-            game_grid[y, x] = 0
-            piece[i] = x+dx, y+dy
-
-        for i in range(len(piece)):
-            x, y = piece[i]
-            game_grid[y, x] = 3
-
-    return not can_move
-
-piece = spawn_piece()
-
-width, height = game_width*cell_size, game_height*cell_size
+width, height = tetris.width*cell_size, tetris.height*cell_size
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Tetris')
 
@@ -115,32 +27,26 @@ colors = [
 black = colors[0]
 white = colors[1]
 
-def draw_grid():
-    for i in range(game_grid.shape[0]-top_margin):
-        for j in range(game_grid.shape[1]):
-            val = game_grid[i+top_margin, j]
-            if val != 0:
-                pygame.draw.rect(win, colors[val-1], (j*cell_size, i*cell_size, cell_size, cell_size))
-
 clock = pygame.time.Clock()
 
+points = 0
 running = True
 paused = False
 while running:
     clock.tick(10)
+
+    action = 'nothing'
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
             if event.key == pygame.K_UP:
-                rotate_piece(piece)
-            if event.key == pygame.K_DOWN:
-                move_piece(piece, 0, 1)
+                action = 'up'
             if event.key == pygame.K_LEFT:
-                move_piece(piece, -1, 0)
+                action = 'left'
             if event.key == pygame.K_RIGHT:
-                move_piece(piece, 1, 0)
+                action = 'right'
             if event.key == pygame.K_SPACE:
                 paused = not paused
         if event.type == pygame.QUIT:
@@ -149,33 +55,17 @@ while running:
     if paused:
         continue
 
-    if move_piece(piece, 0, 1):
-        piece = spawn_piece()
-
-        points = 0
-        # проверяем заполнилась ли какая-то строчка
-        i = game_grid.shape[0]-1
-        while i >= top_margin:
-            row = game_grid[i, :]
-            if np.sum(row) == 0:
-                break
-            if np.all(row > 0):
-                points += 10
-                game_grid[top_margin:i+1, :] = game_grid[top_margin-1:i, :]
-            else:
-                i -= 1
-        if points>0: print(f'Got {points} points!')
-
-        if np.sum(game_grid[top_margin]) > 0:
-            print('Game over!')
-            paused = True
-
-    # print(piece)
+    grid, reward, stopped = tetris.step(TetrisGame.action_names[action])
+    points += reward
 
     win.fill(black)
-
-    draw_grid()
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            if grid[i, j] != 0:
+                pygame.draw.rect(win, colors[grid[i, j]-1], (j*cell_size, i*cell_size, cell_size, cell_size))
 
     pygame.display.flip()
+
+print(f'You scored {points} points!')
 
 pygame.quit()
---
+++ tetris/game.py
@@ -0,0 +1,145 @@
+import numpy as np
+import random
+
+from typing import Tuple, Optional
+
+class TetrisGame:
+
+    action_names = {
+        'nothing': 0,
+        'left': 1,
+        'right': 2,
+        'up': 3
+    }
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.top_margin = 4
+        self.grid = np.zeros((self.top_margin+height, width), dtype=np.int32)
+
+        self._spawn_piece()
+
+    def step(self, action: int) -> Tuple[np.ndarray, int, bool]:
+        points = 0
+        finished = False
+
+        if action == 1:
+            self._move_piece(-1, 0)
+        if action == 2:
+            self._move_piece(1, 0)
+        if action == 3:
+            self._rotate_piece()
+
+        if self._move_piece(0, 1):
+            self._spawn_piece()
+
+            points = 0
+
+            i = self.grid.shape[0]-1
+            while i >= self.top_margin:
+                row = self.grid[i, :]
+                if np.sum(row) == 0:
+                    break
+                if np.all(row > 0):
+                    points += 1
+                    self.grid[self.top_margin:i+1, :] = self.grid[self.top_margin-1:i, :]
+                else:
+                    i -= 1
+
+            if np.sum(self.grid[self.top_margin]) > 0:
+                paused = True
+
+        return self.grid[self.top_margin:], points, finished
+
+    def reset(self, seed:Optional[int] = None):
+        random.seed(seed)
+
+        self.grid = np.zeros((self.top_margin+height, width), dtype=np.int32)
+        self._spawn_piece()
+
+    def _spawn_piece(self):
+        piece_shapes = [
+            # квадратик
+            [[1, 1],
+             [1, 1]],
+            # ступенька вправо
+            [[0, 1, 1],
+             [1, 1]],
+            # ступенька влево
+            [[1, 1],
+             [0, 1, 1]],
+            # палочка
+            [[1, 1, 1, 1]],
+            # палочка с крюком справа
+            [[0, 0, 1],
+             [1, 1, 1]],
+            # палочка с крюком слева
+            [[1, 0, 0],
+             [1, 1, 1]],
+            # треугольник
+            [[0, 1, 0],
+             [1, 1, 1]]
+        ]
+
+        sx, sy = self.width//2, self.top_margin-1
+        shape = random.choice(piece_shapes)
+
+        self.piece = []
+        for i in range(len(shape)):
+            for j in range(len(shape[i])):
+                if shape[i][j] != 0:
+                    self.piece.append((sx+j, sy+i))
+
+    def _piece_pos(self):
+        x = sum([b[0] for b in self.piece]) / len(self.piece)
+        y = sum([b[1] for b in self.piece]) / len(self.piece)
+        return int(x), int(y)
+
+    def _rotate_piece(self):
+        mx, my = self._piece_pos()
+
+        new_piece = [(mx + (y-my), my + (mx-x)) for x, y in self.piece]
+        for x, y in new_piece:
+            if x >= self.width or x < 0 or y >= self.height:
+                return
+            # if not (x, y) in new_piece and grid[y, x] != 0:
+            #     return
+
+        for i in range(len(self.piece)):
+            x, y = self.piece[i]
+            self.grid[y, x] = 0
+
+        for i in range(len(self.piece)):
+            x, y = self.piece[i]
+            self.piece[i] = (mx + (y-my), my + (mx-x))
+
+        for i in range(len(self.piece)):
+            x, y = self.piece[i]
+            self.grid[y, x] = 3
+
+    def _move_piece(self, dx, dy) -> bool:
+        min_x = np.min([b[0] for b in self.piece])
+        max_x = np.max([b[0] for b in self.piece])
+        max_y = np.max([b[1] for b in self.piece])
+        if max_x+dx > self.width-1 or min_x+dx < 0:
+            return False
+        if max_y >= self.height+self.top_margin-1:
+            return True
+
+        can_move = True
+        for x, y in self.piece:
+            if not (x+dx, y+dy) in self.piece and self.grid[y+dy, x+dx] != 0:
+                can_move = False
+                break
+        if can_move:
+            for i in range(len(self.piece)):
+                x, y = self.piece[i]
+                self.grid[y, x] = 0
+                self.piece[i] = x+dx, y+dy
+
+            for i in range(len(self.piece)):
+                x, y = self.piece[i]
+                self.grid[y, x] = 3
+
+        return not can_move
